d73c02bd59afb0d29fe4d2e3e69927665d865def
mikaelapisani <mikaela.pisani@rootstrap.com>
2022-11-02 23:07:11 -0400

    add changes

--- start_biofeedback.sh
+++ start_biofeedback.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-export PORT=8080
-npm start 
+
+echo 'killing biofeedback'
+ID=$(ps faux | grep python | grep biofeedback | awk '{print $2}')
+kill -9
+sleep 10
+rm -f /tmp/biofeedback.log
+
+echo 'starting biofeedback'
+
+cd /home/pi/biofeedback.ant.net.uy; export PORT=8080; npm start >> /tmp/biofeedback.log 2>&1 & 
